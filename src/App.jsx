7b062ba6a2ef48dc13615132d34e57669df63c38
Nilesh <nileshpatil1111-5@gmail.com>
2024-04-13 18:35:40 +0530

    set up base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-function App() {
+import Dashboard from "./Page/Dashboard"
 
-  return (
-    <>
-     <h1 className='bg-red-300' >ok</h1>
-     <p className='bg-slate-50'> grest</p>
-    </>
-  )
-}
+ function App() {
+// const [data,setdatat]=useState([])
+  
+// async function fetdata(){
+// try{
+  
+//   const response=await fetch('https://5a44d073-1bc7-4f5f-945f-08d77628416c.mock.pstmn.io/bag-images')
+//  cdata=await response.json()
+//  setdatat(data)
+// }catch(err){
+//   console.log(err)
+// }
+// }
+// fetdata()
 
+//   return (
+//     <div className='App'>
+//       <h1>Hello React.</h1>
+//       {data?.map(item=><li key={item.image}>{item?.title}</li>)}
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// } 
+
+// // Log to consol
+
+return(
+  < div className="flex justify-center">
+  <Dashboard/>
+  </div>
+)
+ }
 export default App
